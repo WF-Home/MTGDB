87a4b32151737c9ee9223c833ef862c541bcf06b
WF-Home <wiljay.dev@gmail.com>
2026-02-10 18:48:10 -0500

    Updated styling + added comments to javascript

--- public/assets/js/cards.js
+++ public/assets/js/cards.js
@@ -2,7 +2,6 @@ const api = "https://api.scryfall.com/cards/search"
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-
 const set = urlParams.get('set'); 
 const setName = urlParams.get('name');
 
@@ -10,12 +9,16 @@ window.addEventListener("load", () => {
     getCards();
 })
 
+// Fetch Card list based on set
+
 async function getCards() {
     const URI = `e:${set}`
     const response = await fetch(`${api}?q=${encodeURIComponent(URI)}`);
     const data = await response.json();
     generateCards(data.data)
 }
+
+// Generates Cards
 
 function generateCards(cards) {
 
@@ -28,7 +31,7 @@ function generateCards(cards) {
 
     cards.forEach( card => {
         if (Object.hasOwn(card, 'image_uris')) {
-              template += `<img src="${card.image_uris.small}">`;
+              template += `<img class="imgCards" src="${card.image_uris.large}">`;
         }
     });
 
--- public/assets/js/sets.js
+++ public/assets/js/sets.js
@@ -1,18 +1,27 @@
 const api = "https://api.scryfall.com/sets/"
 
+// Fetch Api Call
+
 async function getSets() {
     const response = await fetch(api);
     const obj = await response.json();
 
+    // Filtering any sets that has less than 20 cards
+
     const filteredData = obj.data.filter( (set) => set.card_count > 20)
     console.log(filteredData)
 
+    // Grouping sets by their release year
     const groupedSets = Object.groupBy(filteredData, ({ released_at }) => new Date(released_at).getFullYear());
 
     generateSetSelection(groupedSets);
 }
 
+// Generate the html for the sets
+
 function generateSetSelection(sets) {
+
+    // Sorts the years by descending order
 
     const orderedYears = Object.keys(sets).sort((a,b) => b - a);
 
@@ -26,6 +35,9 @@ function generateSetSelection(sets) {
                 <h2>${year}</h2>
                 <section class="sets-container">
         `
+
+        // Sorts the sets within the years by most recent releases 
+
         const sortedSet = sets[year].sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate))
         sortedSet.forEach( set => {
             template += createSetButton(set);
@@ -41,9 +53,14 @@ function generateSetSelection(sets) {
 
 }
 
+// Creates the set buttons
+
 function createSetButton(set) {
     return `
-        <a class="sets-button" href="/cards.html?set=${set.code}&name=${set.name}">${set.name}</a>
+        <div class="sets-button">
+            <img src="${set.icon_svg_uri}"> 
+            <a href="/cards.html?set=${set.code}&name=${set.name}">${set.name}</a>
+        </div>
     `
 }
 
