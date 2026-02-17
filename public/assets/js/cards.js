7889d11ef8e5b47b6a1d6e99320ad999e90200de
WF-Home <wiljay.dev@gmail.com>
2026-02-16 20:07:29 -0500

    Created card details view

--- public/assets/js/cards.js
+++ public/assets/js/cards.js
@@ -21,17 +21,35 @@ async function getCards() {
 // Generates Cards
 
 function generateCards(cards) {
-
+    let main = document.querySelector("main")
     let cardListView = document.getElementById("cards-list-view");
     let title = document.createElement("h1")
     let cardContainer = document.createElement("section")
 
-    cardContainer.setAttribute("id", "card-container")
+    cardContainer.setAttribute("id", "cards-container")
 
     title.innerHTML = setName
 
-    cardListView.append(title);
-    cardListView.append(cardContainer);
+    cardListView.append(title)
+    cardListView.append(cardContainer)
+
+    let cardDetailsView = document.createElement("section")
+    cardDetailsView.setAttribute("id", "cardDetailsView")
+
+    main.append(cardDetailsView)
+
+    let cardDetailsContainer = document.createElement("article")
+    cardDetailsContainer.setAttribute("id", "cardDetailsContainer")
+
+    let closeButton = document.createElement("button")
+    closeButton.addEventListener("click", function() {
+        cardDetailsView.classList.remove("isDisplayed")
+    })
+    
+    cardDetailsView.append(cardDetailsContainer)
+    cardDetailsView.append(closeButton)
+
+    closeButton.innerHTML = "Go Back To List"
 
     cards.forEach( card => {
         if (Object.hasOwn(card, 'image_uris')) {
@@ -42,10 +60,27 @@ function generateCards(cards) {
             cardObj.setAttribute("src", card.image_uris.small)
             cardObj.addEventListener("click", function() {
                 console.log(card)
+                cardDetailsContainer.innerHTML = cardDetails(card)
+                cardDetailsView.classList.add("isDisplayed")
             })
 
             cardContainer.append(cardObj)
 
         }
     });
+}
+
+function cardDetails(card) {
+    return `
+        <img src=${card.image_uris.normal}>
+        <section>
+            <div>
+                <h1>${card.name}</h1>
+                <p class="type">${card.type_line}</p>
+            </div>
+            <p class="oracle">${card.oracle_text}</p>
+            <p class="flavour">${card.flavor_text ? card.flavor_text : ""}</p>
+            <p class="artist">Artist: ${card.artist}</p>
+        <section>
+    `
 }
